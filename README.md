214bf46b9a3621c5e2c3a59d2566341134582bf4
Harsh Gupta <41520545+Harshgupta718@users.noreply.github.com>
2021-08-16 15:46:20 +0530

    app uploaded

---
+++ README.md
@@ -0,0 +1,1 @@
+This app is built as an assignment for Yellow Class. Hope u like it.
---
+++ lib/data/core/api_constants.dart
@@ -0,0 +1,7 @@
+class ApiConstants {
+  ApiConstants._();
+
+  static const String BASE_URL = "https://api.themoviedb.org/3/";
+  static const String API_KEY = "64fa32e0d8e1cb0d288f57c351f8cb36";
+  static const String BASE_IMAGE_URL = "https://image.tmdb.org/t/p/w500";
+}
